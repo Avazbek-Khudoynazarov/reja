87e50dd32c485555a7613a4bd9a898b58ce3aa55
Avazbek-khudoynazarov <avazbek.khudoynazarov@bk.ru>
2023-01-05 03:28:18 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+if(err) {
+console.log("ERROR", err);
+}
+else {
+    user = JSON.parse(data)
+}
+});
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.set("view","view");
+app.set("views","views");
 app.set("view engine", "ejs");
 
-app.get("/hello", function( req, res) {
-    res.end(`<h1>HELLO WORLD</h1>`);
-});
-app.get("/gift", function( req, res) {
-    res.end(`<h1>you have entered the gifts page</h1>`);
-});
+app.post("/create-item", (req, res) => {
+console.log(req.body);
+res.json({test: "success"});
+})
+
+app.get("/harid", function(req, res) {
+    res.render("harid");
+})
+
+app.get("/author",function(req, res)  {
+    res.render("author", {user: user});
+})
 
 
 const server = http.createServer(app);
