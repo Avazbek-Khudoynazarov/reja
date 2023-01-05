e8639dd65d4b3af379cf89fb37d3dd109b0e94ab
Avazbek-khudoynazarov <avazbek.khudoynazarov@bk.ru>
2023-01-05 18:42:38 +0900

    left frame created

--- server.js
+++ server.js
@@ -5,37 +5,35 @@ const fs = require("fs");
 
 let user;
 fs.readFile("database/user.json", "utf8", (err, data) => {
-if(err) {
-console.log("ERROR", err);
-}
-else {
-    user = JSON.parse(data)
-}
+  if (err) {
+    console.log("ERROR", err);
+  } else {
+    user = JSON.parse(data);
+  }
 });
 
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.set("views","views");
+app.set("views", "views");
 app.set("view engine", "ejs");
 
 app.post("/create-item", (req, res) => {
-console.log(req.body);
-res.json({test: "success"});
-})
+  console.log(req.body);
+  res.json({ test: "success" });
+});
 
-app.get("/harid", function(req, res) {
-    res.render("harid");
-})
+app.get("/harid", function (req, res) {
+  res.render("harid");
+});
 
-app.get("/author",function(req, res)  {
-    res.render("author", {user: user});
-})
-
+app.get("/author", function (req, res) {
+  res.render("author", { user: user });
+});
 
 const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, function() {
-    console.log(`server is succesfully running: ${PORT}`);
-})
+server.listen(PORT, function () {
+  console.log(`server is succesfully running: ${PORT}`);
+});
