82bfb432b3482c88389d0873895fcf61d9e7939a
Avazbek-khudoynazarov <avazbek.khudoynazarov@bk.ru>
2023-01-15 04:59:24 +0900

    I-task

--- training.js
+++ training.js
@@ -1,6 +1,12 @@
-console.log('training page ishga tushdi');
-const { kopaytirish } = require("./calculator");
-const hisoblash = require("./calculator");
+const ints = [0, -15, 5, -3, 2, -8, 9, -11];
+const events = ints.filter(function (item) {
+  return item > 0;
+});
+console.log(events);
 
-const natija = hisoblash.kopaytirish(4 , 7);
-    console.log('natija', natija);
+// console.log("training page ishga tushdi");
+// const { kopaytirish } = require("./calculator");
+// const hisoblash = require("./calculator");
+
+// const natija = hisoblash.kopaytirish(4, 7);
+// console.log("natija", natija);
